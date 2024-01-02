e62640b4a47c6cfdee58a6af6dd64fb71d9fb1a7
Bhushan Jadhav <officialbhushan2020@gmail.com>
2024-01-02 12:15:05 +0530

    Day 2 solution added.

---
+++ solutions/day2.py
@@ -0,0 +1,6 @@
+bill_amount = int(input("Enter Bill amount: "))
+tip = bill_amount * 0.2
+print(float(tip))
+
+#Explanation: 20% of 50 is 10. 
+#To calculate 20% of a given amount, you can multiply the number by 20 and divide it by 100: 50*(20/100) = 10.0
