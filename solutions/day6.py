926cd5970e5cd3e9bb2b76e6b6038c67cef609ac
Bhushan Jadhav <officialbhushan2020@gmail.com>
2024-01-06 22:04:39 +0530

    Day 6 (Revision) solution added.

---
+++ solutions/day6.py
@@ -0,0 +1,43 @@
+print("AFFIRMATIONS TODAY.")
+print()
+print("Hello user, I am mimo. What's your name?:")
+name = input()
+print("Hi," + name + "!. How are you doing today?")
+feeling = input()
+if feeling == "Good" or "good":
+  print("That's awesome! What day is it today?")
+else:
+  print("Oh, I hear you. Let's cheer you up! What day is today?")
+day = input()
+if day == "Monday" or day == "monday":
+  print("Manifesting Monday, it is!")
+  print()
+  print("Each morning when I open my eyes I say to myself: I, not events, have the power to make me happy or unhappy today. I can choose which it shall be. Yesterday is dead, tomorrow hasn’t arrived yet. I have just one day, today, and I’m going to be happy in it.")
+elif day == "Tuesday" or day == "tuesday":
+  print("Oh, It's 'Thoughtful Tuesday'")
+  print()
+  print("Remember,",name,"you are today where your thoughts have brought you; you will be tomorrow where your thoughts take you.")
+elif day == "Wednesday" or day == "wednesday":
+  print("Well, Wellness Wednesday?")
+  print()
+  print("This one will surely make your day.")
+  print("You are always stronger and more resourceful than you give yourself credit for.")
+elif day == "Thursday" or day == "thursday":
+  print("Thankful Thursday today! Here is what you need to know.")
+  print()
+  print("Don’t count the days. Make the days count. Keep going",name,". You are almost there.")
+elif day == "Friday" or day == "friday":
+  print("Finally, Fantastic Friday!")
+  print()
+  print(name,",Failure is the condiment that gives success its flavor. Don't be disheartened by what happened yesterday, be happy for today. You got this!")
+elif day == "Saturday" or day == "saturday":
+  print("Sassy, serene Saturday is here!")
+  print()
+  print("""Come on""",name, """without the weekend, where would the week be?Have fun.""")
+else:
+  print ("Sacred Sunday it is.")
+  print()
+  print(name,"you are a divine vessel of love and light. Let the spirit guides you in every step I take")
+print()
+print("Hope you feel better now. Have a nice day", name ,"!")
+print("Love, mimo ;)")
