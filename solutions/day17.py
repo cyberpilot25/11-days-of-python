a8619df357c90bab523e92d3f98be3fd0233c3f6
Bhushan Jadhav <officialbhushan2020@gmail.com>
2024-01-17 13:19:50 +0530

    Day 17 solution added.

---
+++ solutions/day17.py
@@ -0,0 +1,10 @@
+#learning string function
+msg = input()
+print(msg.replace("#"," "))
+
+#creating first function of our own
+def welcome():
+   user =str(input())
+   print("Welcome," ,user)
+
+welcome()
