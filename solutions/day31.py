393310ea162935bb77530b9566cd62b3321cf3f4
Bhushan Jadhav <officialbhushan2020@gmail.com>
2024-02-14 19:57:00 +0530

    Day 31 solution added.

---
+++ solutions/day31.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+    
+    def intro(self):
+        print(self.name + " (Level " + self.level + ")")
+
+name = input()  
+level = input()
+
+player = Player(name, level)
+player.intro()
